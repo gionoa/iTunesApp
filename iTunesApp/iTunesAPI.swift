bb4a98060e3b8154a3425dfa7a4d95b32c58fd7c
Giovanni Noa <gnoa001@fiu.edu>
2019-08-10 02:34:30 -0400

    Add MVVM folders and create iTunesAPI file

---
+++ iTunesApp/iTunesAPI.swift
@@ -0,0 +1,16 @@
+//
+//  iTunesAPI.swift
+//  iTunesApp
+//
+//  Created by Giovanni Noa on 8/10/19.
+//  Copyright © 2019 Giovanni Noa. All rights reserved.
+//
+
+import Foundation
+
+struct iTunesAPI {
+    
+    static func fetchData(mediaType: MediaType, completion: @escaping ([MediaItem], Error?) -> Void) {
+        
+    }
+}
