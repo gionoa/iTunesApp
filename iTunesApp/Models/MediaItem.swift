8aa2874a0139a7703d0a646f6eb07e99a09b5b74
Giovanni Noa <gnoa001@fiu.edu>
2019-08-10 02:38:25 -0400

    Add MediaType and MediaItem models

---
+++ iTunesApp/Models/MediaItem.swift
@@ -0,0 +1,15 @@
+//
+//  MediaItem.swift
+//  iTunesApp
+//
+//  Created by Giovanni Noa on 8/10/19.
+//  Copyright © 2019 Giovanni Noa. All rights reserved.
+//
+
+import Foundation
+
+struct MediaItem {
+    let title: String
+    let imageURL: String
+    let mediaType: MediaType
+}
---
+++ iTunesApp/Models/MediaType.swift
@@ -0,0 +1,16 @@
+//
+//  MediaType.swift
+//  iTunesApp
+//
+//  Created by Giovanni Noa on 8/10/19.
+//  Copyright © 2019 Giovanni Noa. All rights reserved.
+//
+
+import Foundation
+
+
+enum MediaType: String {
+    case movies = "Movies"
+    case music = "Music"
+    case tvShows = "TV Shows"
+}
